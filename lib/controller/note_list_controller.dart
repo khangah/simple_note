8a826dc855d03bfa0031643f068ccc056d9e63cc
khangah <khangah52@gmail.com>
2022-07-12 04:22:56 +0430

    add NoteListController

---
+++ lib/controller/note_list_controller.dart
@@ -0,0 +1,43 @@
+import 'package:get/get.dart';
+import 'package:note/data/app_data.dart';
+import 'package:note/model/note_model.dart';
+
+class NoteListController extends GetxController {
+  List<NoteModel> list = [];
+
+  String editingText = '';
+
+  @override
+  void onInit() {
+    super.onInit();
+    _loadNoteList();
+  }
+
+  void _loadNoteList() async {
+    list = await AppData.getNoteList();
+    update();
+  }
+
+  void add(String text) {
+    final nowTime = DateTime.now().millisecondsSinceEpoch;
+    list.add(NoteModel(text: text, createTime: nowTime, editTime: nowTime));
+    update();
+  }
+
+  void edit(int i, String text) async {
+    list[i].editTime = DateTime.now().millisecondsSinceEpoch;
+    list[i].text = text;
+    /// this delay just for fun
+    await Future.delayed(const Duration(milliseconds: 200));
+    update();
+  }
+
+  void remove(int i) async {
+    list.removeAt(i);
+    AppData.setNoteList(list);
+    await Future.delayed(const Duration(milliseconds: 400));
+    update();
+  }
+
+
+}
